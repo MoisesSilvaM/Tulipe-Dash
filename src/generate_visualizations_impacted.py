f8273162fa7464f684d68c590f2a09091c4e64fc
Moisés Silva Muñoz <moises.silva@usach.cl>
2024-09-16 23:25:57 +0200

    Update generate_visualizations_impacted.py

--- src/generate_visualizations_impacted.py
+++ src/generate_visualizations_impacted.py
@@ -2,131 +2,174 @@ import plotly.express as px
 import datetime
 import textwrap
 
-def generate_visualizations(street_data_without, street_data_with, traffic_name, traffic_lowercase,  list_timeframe_in_seconds, list_timeframe_string, len_time_intervals_string, geo_data, hideout, dict_names, timeframe_from, timeframe_to, title_size):
+
+def generate_visualizations(street_data_without, street_data_with, traffic_name, traffic_lowercase,
+                            list_timeframe_in_seconds, list_timeframe_string, len_time_intervals_string, geo_data,
+                            hideout, dict_names, timeframe_from, timeframe_to, title_size):
+    """Generate visualizations based on the street data."""
+
+    # If specific streets are selected in the hideout
     if bool(dict_names):
         my_list = []
         for (key, value) in hideout.items():
             for v in value:
                 my_list.append(v)
-        fig = generate_figure(street_data_without, street_data_with, traffic_name, traffic_lowercase, list_timeframe_in_seconds, timeframe_from, timeframe_to, geo_data, my_list, title_size)
+        fig = generate_figure(street_data_without, street_data_with, traffic_name, traffic_lowercase,
+                              list_timeframe_in_seconds, timeframe_from, timeframe_to, geo_data, my_list, title_size)
         return fig
     else:
-        fig = generate_figure_15_most_impacted(street_data_without, street_data_with, traffic_name, traffic_lowercase,  list_timeframe_in_seconds, list_timeframe_string, len_time_intervals_string, geo_data, timeframe_from, timeframe_to, title_size)
+        # Generate figure for the 15 most impacted streets
+        fig = generate_figure_15_most_impacted(street_data_without, street_data_with, traffic_name, traffic_lowercase,
+                                               list_timeframe_in_seconds, list_timeframe_string,
+                                               len_time_intervals_string, geo_data, timeframe_from, timeframe_to,
+                                               title_size)
         return fig
 
 
-def generate_figure(street_data_without, street_data_with, traffic_name, traffic_lowercase, list_timeframe_in_seconds, timeframe_from, timeframe_to, geojson, my_list, title_size):
+def generate_figure(street_data_without, street_data_with, traffic_name, traffic_lowercase, list_timeframe_in_seconds,
+                    timeframe_from, timeframe_to, geojson, my_list, title_size):
+    """Generate a bar plot for selected streets based on the difference between with and without deviations."""
+
+    # Filter the data based on the selected streets
     df_without = street_data_without[street_data_without.columns.intersection(list_timeframe_in_seconds)].copy()
     df_with = street_data_with[street_data_with.columns.intersection(list_timeframe_in_seconds)].copy()
     df_without = df_without[df_without.index.isin(my_list)]
     df_with = df_with[df_with.index.isin(my_list)]
 
+    # Calculate the mean for each street
     df_without['mean'] = df_without.mean(axis=1)
     df_with['mean'] = df_with.mean(axis=1)
 
+    # Merge and calculate the difference
     df = df_without.merge(df_with, left_index=True, right_index=True, how="left")
     df['difference'] = df['mean_y'].sub(df['mean_x'], axis=0)
 
+    # Apply appropriate transformation for time-based values
     if traffic_lowercase == 'time loss (seconds)' or traffic_lowercase == 'travel time (seconds)' or traffic_lowercase == 'waiting time (seconds)':
         df['diff_dates'] = df['difference'].apply(get_sec_to_date)
     else:
         df['diff_dates'] = df['difference'].apply(get_copy_sec)
+
     index_names = df.index.values.tolist()
     list_names = []
+
     title = 'Difference of the streets in terms of ' + traffic_name + ' for the time interval ' + timeframe_from + ' to ' + timeframe_to
-    wrapped_title = textwrap.wrap(title, width=title_size)
-    wrapped_title_with_br = '<br>'.join(wrapped_title)
-    title_font_size = 16
-    margin = 100
-    if title_size == 40:
-        margin = 140
-    if title_size == 30:
-        title_font_size = 12
-        margin = 160
+
+    # Wrap the title text for better readability
+    wrapped_title = '<br>'.join(textwrap.wrap(title, width=title_size))
+
+    # Adjust title font size based on title size
+    title_font_size, margin = adjust_title_formatting(title_size)
+
+    # Map street names to the plot's x-axis
     for elem in index_names:
         for i in geojson['features']:
             if i["properties"].get("id") == elem:
                 list_names.append(i["properties"].get("name") + ' (id:' + i["properties"].get("id") + ')')
+
+    # generate bar plot
     fig = px.bar(df, y='difference', x=df.index, orientation='v', text='diff_dates',
                  # color='diff_dates',
-                 title=wrapped_title_with_br)
+                 title=wrapped_title)
     if traffic_lowercase == 'time loss (seconds)' or traffic_lowercase == 'travel time (seconds)' or traffic_lowercase == 'waiting time (seconds)':
         fig.update_traces(texttemplate='%{text}', textposition='outside')
     else:
         fig.update_traces(texttemplate='%{text:.2f}', textposition='outside')
-    fig.update_layout(xaxis_title_text='Street')
-    fig.update_layout(yaxis_title_text='Difference in ' + get_traffic_y_axis(traffic_name))
-    fig.update_layout(template='plotly_dark', font=dict(color='#deb522'))
-    fig.update_layout(xaxis=dict(tickmode='array', tickvals=df.index, ticktext=list_names))
-    fig.update_layout(yaxis=dict(showticklabels=False))
-    fig.update_layout(title={'y': 0.95, 'pad': {'b': 50}}, title_font_size=title_font_size)
-    fig.update_layout(autosize=True, margin=dict(t=margin))
+
+    # Update the layout and title
+    fig.update_layout(
+        xaxis_title_text='Street',
+        yaxis_title_text='Difference in ' + get_traffic_y_axis(traffic_name),
+        template='plotly_dark',
+        font=dict(color='#deb522'),
+        xaxis=dict(tickmode='array',
+                   tickvals=df.index,
+                   ticktext=list_names),
+        yaxis=dict(showticklabels=False),
+        title={'y': 0.95, 'pad': {'b': 50}},
+        title_font_size=title_font_size,
+        autosize=True, margin=dict(t=margin))
     return fig
 
 
-def generate_figure_15_most_impacted(street_data_without, street_data_with, traffic_name, traffic_lowercase, list_timeframe_in_seconds, list_timeframe_string, len_time_intervals_string, geo_data, timeframe_from, timeframe_to, title_size):
+def generate_figure_15_most_impacted(street_data_without, street_data_with, traffic_name, traffic_lowercase,
+                                     list_timeframe_in_seconds, list_timeframe_string, len_time_intervals_string,
+                                     geo_data, timeframe_from, timeframe_to, title_size):
+    """Generate a bar plot for the 15 most impacted streets based on the difference between with and without deviations."""
     df_without = street_data_without[street_data_without.columns.intersection(list_timeframe_in_seconds)].copy()
     df_with = street_data_with[street_data_with.columns.intersection(list_timeframe_in_seconds)].copy()
 
+    # Calculate mean and difference
     df_without['mean'] = df_without.mean(axis=1)
     df_with['mean'] = df_with.mean(axis=1)
-
     df = df_without.merge(df_with, left_index=True, right_index=True, how="left")
     df['difference'] = df['mean_y'].sub(df['mean_x'], axis=0)
+
+    # Sort and select top 15 impacted streets
     df = df.sort_values(by=['difference'], ascending=False).head(15)
 
+    # Apply time transformation
     if traffic_lowercase == 'time loss (seconds)' or traffic_lowercase == 'travel time (seconds)' or traffic_lowercase == 'waiting time (seconds)':
         df['diff_dates'] = df['difference'].apply(get_sec_to_date)
     else:
         df['diff_dates'] = df['difference'].apply(get_copy_sec)
     index_names = df.index.values.tolist()
     list_names = []
-    title='15 most impacted streets in terms of ' + traffic_name + ' for the time interval ' + timeframe_from + ' to ' + timeframe_to
-    wrapped_title = textwrap.wrap(title, width=title_size)
-    wrapped_title_with_br = '<br>'.join(wrapped_title)
-    title_font_size = 16
-    margin = 100
-    if title_size == 40:
-        margin = 140
-    if title_size == 30:
-        title_font_size = 12
-        margin = 160
+
+    title = '15 most impacted streets in terms of ' + traffic_name + ' for the time interval ' + timeframe_from + ' to ' + timeframe_to
+
+    # Wrap the title text for better readability
+    wrapped_title = '<br>'.join(textwrap.wrap(title, width=title_size))
+
+    # Adjust title font size and margin based on title size
+    title_font_size, margin = adjust_title_formatting(title_size)
+
+    # Map street names to the plot's x-axis
     for elem in index_names:
         for i in geo_data['features']:
             if i["properties"].get("id") == elem:
                 list_names.append(i["properties"].get("name") + ' (id:' + i["properties"].get("id") + ')')
+
+    # generate bar plot
     fig = px.bar(df, y='difference', x=df.index, orientation='v', text='diff_dates',
                  # color='diff_dates',
-                 title=wrapped_title_with_br
+                 title=wrapped_title
                  )
     if traffic_lowercase == 'time loss (seconds)' or traffic_lowercase == 'travel time (seconds)' or traffic_lowercase == 'waiting time (seconds)':
         fig.update_traces(texttemplate='%{text}', textposition='outside')
     else:
         fig.update_traces(texttemplate='%{text:.2f}', textposition='outside')
-    fig.update_layout(xaxis_title_text='Street')
-    fig.update_layout(yaxis_title_text='Difference in ' + get_traffic_y_axis(traffic_name))
-    fig.update_layout(template='plotly_dark', font=dict(color='#deb522'))
-    fig.update_layout(xaxis=dict(tickmode='array', tickvals=df.index, ticktext=list_names, tickangle=90))
-    fig.update_layout(yaxis=dict(showticklabels=False))
-    fig.update_layout(title={'y': 0.95, 'pad': {'b': 50}}, title_font_size=title_font_size)
-    fig.update_layout(autosize=True, margin=dict(t=margin))
+
+    # Update the layout and title
+    fig.update_layout(
+        xaxis_title_text='Street',
+        yaxis_title_text='Difference in ' + get_traffic_y_axis(traffic_name),
+        template='plotly_dark',
+        font=dict(color='#deb522'),
+        xaxis=dict(tickmode='array',
+                   tickvals=df.index,
+                   ticktext=list_names,
+                   tickangle=90),
+        yaxis=dict(showticklabels=False),
+        title={'y': 0.95, 'pad': {'b': 50}},
+        title_font_size=title_font_size,
+        autosize=True, margin=dict(t=margin))
     return fig
 
 
 def get_sec_to_date(seconds):
-    if seconds < 0:
-        sec = abs(seconds)
-        dates = str(datetime.timedelta(seconds=int(sec)))
-    else:
-        dates = str(datetime.timedelta(seconds=int(seconds)))
-    return dates
+    """Convert seconds to a time delta string (hh:mm:ss)."""
+    sec = abs(seconds)
+    return str(datetime.timedelta(seconds=int(sec)))
 
 
 def get_copy_sec(seconds):
+    """Return the seconds value as is for non-time-based metrics."""
     return seconds
 
 
 def get_traffic_y_axis(traffic):
+    """Return the label for the y-axis based on the traffic indicator."""
     if traffic == "vehicle density (vehicles/kilometres)":
         inf = "density (vehicles/kilometres)"
     elif traffic == "vehicle occupancy (%)":
@@ -147,9 +190,22 @@ def get_traffic_y_axis(traffic):
 
 
 def get_response(traffic):
+    """Determine if the traffic metric requires conversion to time format."""
     inf = False
     if traffic == 'time loss (seconds)':
         inf = True
     elif traffic == 'travel time (seconds)':
         inf = True
     return inf
+
+
+def adjust_title_formatting(title_size):
+    """Adjust the title font size and margin based on the provided title size."""
+    title_font_size = 16
+    margin = 100
+    if title_size == 40:
+        margin = 140
+    elif title_size == 30:
+        title_font_size = 12
+        margin = 160
+    return title_font_size, margin
