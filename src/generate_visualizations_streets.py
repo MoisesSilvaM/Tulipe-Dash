6910afc3b3c078e199447325f5d8cec6c4fe4082
Moisés Silva Muñoz <moises.silva@usach.cl>
2024-09-16 23:26:27 +0200

    Update generate_visualizations_streets.py

--- src/generate_visualizations_streets.py
+++ src/generate_visualizations_streets.py
@@ -5,18 +5,23 @@ import textwrap
 def generate_visualizations(street_data_without, street_data_with, traffic_name, traffic, dict_names,
                             list_timeframe_in_seconds, list_timeframe_string, len_time_intervals_string, timeframe_from,
                             timeframe_to, title_size):
-    if bool(dict_names):
+    """Generate visualizations comparing street data with and without deviations over a specified timeframe."""
+
+    if bool(dict_names):  # Check if any specific streets are selected
         if len(dict_names) == 1:
+            # If a single street is selected, generate a specific figure for it
             fig = generate_figure1(street_data_without, street_data_with, traffic_name, traffic, dict_names,
                                    list_timeframe_in_seconds, list_timeframe_string, len_time_intervals_string,
                                    timeframe_from, timeframe_to, title_size)
             return fig
         else:
+            # If multiple streets are selected, generate a comparative figure for them
             fig = generate_figure_some(street_data_without, street_data_with, traffic_name, traffic, dict_names,
                                        list_timeframe_in_seconds, list_timeframe_string, len_time_intervals_string,
                                        timeframe_from, timeframe_to, title_size)
             return fig
     else:
+        # If no specific streets are selected, generate a figure for all streets
         mean_street_data_without = street_data_without.mean()
         mean_street_data_with = street_data_with.mean()
         fig = generate_figure_all(mean_street_data_without, mean_street_data_with, traffic_name, traffic,
@@ -28,60 +33,69 @@ def generate_visualizations(street_data_without, street_data_with, traffic_name,
 def generate_figure1(street_data_without, street_data_with, traffic_name, traffic, dict_names,
                      list_timeframe_in_seconds, list_timeframe_string, len_time_intervals_string, timeframe_from,
                      timeframe_to, title_size):
+    """Generate a figure comparing data for a single selected street over time."""
+
     name = ''
     fig1 = go.Figure()
-    for (key, value) in dict_names.items():
-        k = key
-        name = value + ' (id:' + key + ')'
-        street_data_without = street_data_without.loc[k]
-        street_data_with = street_data_with.loc[k]
+    # Get the street data from the dictionary of selected streets
+    for key, value in dict_names.items():
+        street_data_without = street_data_without.loc[key]
+        street_data_with = street_data_with.loc[key]
+        name = f'{value} (id:{key})'
+
+    # Filter data for selected timeframes
     if len(list_timeframe_in_seconds) != len_time_intervals_string:
         street_data_without = street_data_without.loc[street_data_without.index.isin(list_timeframe_in_seconds)]
         street_data_with = street_data_with.loc[street_data_with.index.isin(list_timeframe_in_seconds)]
         title = 'Comparing the ' + traffic_name + ' for the vehicles that originally passed through ' + name + ' for the time interval ' + timeframe_from + ' to ' + timeframe_to
     else:
         title = 'Comparing the ' + traffic_name + ' for the vehicles that originally passed through ' + name + ' for all the time intervals'
-    wrapped_title = textwrap.wrap(title, width=title_size)
-    wrapped_title_with_br = '<br>'.join(wrapped_title)
-    title_font_size = 16
-    margin = 100
-    if title_size <= 40:
-        title_font_size = 12
-        margin = 160
+
+    # Wrap the title text for better readability
+    wrapped_title = '<br>'.join(textwrap.wrap(title, width=title_size))
+
+    # Adjust title font size and margin based on title size
+    title_font_size = 12 if title_size <= 40 else 16
+    margin = 160 if title_size <= 40 else 100
+
+    # Plot the data
     fig1.add_trace(go.Scatter(x=street_data_without.index, y=street_data_without.values,
                               mode='lines+markers',
                               name='without deviations'))
     fig1.add_trace(go.Scatter(x=street_data_with.index, y=street_data_with.values,
                               mode='lines+markers',
                               name='with deviations'))
-    fig1.update_layout(yaxis_title=traffic)
+
+    # Update the layout and title
     fig1.update_layout(
-        title_text=wrapped_title_with_br,
+        yaxis_title=traffic,
+        title_text=wrapped_title,
         xaxis_title_text='Time interval',
-    )
-    fig1.update_layout(
         xaxis=dict(
             tickmode='array',
             tickvals=street_data_without.index,
-            ticktext=list_timeframe_string)
-    )
-    fig1.update_layout(title={'y': 0.95, 'pad': {'b': 50}}, title_font_size=title_font_size)
-    fig1.update_layout(autosize=True, margin=dict(t=margin))
-    fig1.update_layout(template='plotly_dark', font=dict(color='#deb522'))
+            ticktext=list_timeframe_string),
+        title={'y': 0.95, 'pad': {'b': 50}}, title_font_size=title_font_size,
+        autosize=True, margin=dict(t=margin),
+        template='plotly_dark',
+        font=dict(color='#deb522'))
     return fig1
 
 
 def generate_figure_some(street_data_without, street_data_with, traffic_name, traffic, dict_names,
                          list_timeframe_in_seconds, list_timeframe_string, len_time_intervals_string, timeframe_from,
                          timeframe_to, title_size):
+    """Generate a figure comparing data for multiple selected streets over time."""
+
     title = ''
     fig1 = go.Figure()
-    for (key, value) in dict_names.items():
-        k = key
-        name = value + ' (id:' + key + ')'
-        df_without = street_data_without.loc[k]
-        df_with = street_data_with.loc[k]
+    # Iterate through the selected streets
+    for key, value in dict_names.items():
+        df_without = street_data_without.loc[key]
+        df_with = street_data_with.loc[key]
+        name = f'{value} (id:{key})'
 
+        # Filter data for selected timeframes
         if len(list_timeframe_in_seconds) != len_time_intervals_string:
             df_without = df_without.loc[df_without.index.isin(list_timeframe_in_seconds)]
             df_with = df_with.loc[df_with.index.isin(list_timeframe_in_seconds)]
@@ -92,6 +106,7 @@ def generate_figure_some(street_data_without, street_data_with, traffic_name, tr
             title = 'Comparing the ' + traffic_name + (' for the vehicles that originally passed through some ' +
                                                        'streets for all the time intervals')
 
+        # Plot the data for each street
         fig1.add_trace(go.Scatter(x=df_without.index, y=df_without.values,
                                   mode='lines+markers',
                                   name=name + '<br>without deviations'))
@@ -99,30 +114,37 @@ def generate_figure_some(street_data_without, street_data_with, traffic_name, tr
                                   mode='lines+markers',
                                   name=name + '<br>with deviations'))
         fig1.update_layout(yaxis_title=traffic)
-    wrapped_title = textwrap.wrap(title, width=title_size)
-    wrapped_title_with_br = '<br>'.join(wrapped_title)
-    title_font_size = 16
-    if title_size <= 40:
-        title_font_size = 12
+
+    # Wrap the title text for better readability
+    wrapped_title = '<br>'.join(textwrap.wrap(title, width=title_size))
+
+    # Adjust title font size based on title size
+    title_font_size = 16 if title_size <= 40 else 12
+
+    # Update the layout and title
     fig1.update_layout(
-        title_text=wrapped_title_with_br,
+        title_text=wrapped_title,
         xaxis_title_text='Time interval',
-    )
-    fig1.update_layout(
         xaxis=dict(
             tickmode='array',
             tickvals=df_without.index,
-            ticktext=list_timeframe_string)
+            ticktext=list_timeframe_string),
+        title={'y': 0.95, 'pad': {'b': 50}},
+        title_font_size=title_font_size,
+        autosize=True,
+        margin=dict(t=100),
+        template='plotly_dark',
+        font=dict(color='#deb522')
     )
-    fig1.update_layout(title={'y': 0.95, 'pad': {'b': 50}}, title_font_size=title_font_size)
-    fig1.update_layout(autosize=True, margin=dict(t=100))
-    fig1.update_layout(template='plotly_dark', font=dict(color='#deb522'))
     return fig1
 
 
 def generate_figure_all(mean_street_data_without, mean_street_data_with, traffic_name, traffic,
                         list_timeframe_in_seconds, list_timeframe_string, len_time_intervals_string, timeframe_from,
                         timeframe_to, title_size):
+    """Generate a figure comparing the average data of all streets over time."""
+
+    # Filter data for selected timeframes
     if len(list_timeframe_in_seconds) != len_time_intervals_string:
         mean_street_data_without = mean_street_data_without.loc[
             mean_street_data_without.index.isin(list_timeframe_in_seconds)]
@@ -131,7 +153,9 @@ def generate_figure_all(mean_street_data_without, mean_street_data_with, traffic
     #
     else:
         title = 'Comparing the average ' + traffic_name + ' on all the streets for all the time intervals'
+
     fig1 = go.Figure()
+    # Plot the data for each street
     fig1.add_trace(go.Scatter(x=mean_street_data_without.index, y=mean_street_data_without.values,
                               mode='lines+markers',
                               name='without deviations'))
@@ -139,23 +163,25 @@ def generate_figure_all(mean_street_data_without, mean_street_data_with, traffic
                               mode='lines+markers',
                               name='with deviations'))
 
-    wrapped_title = textwrap.wrap(title, width=title_size)
-    wrapped_title_with_br = '<br>'.join(wrapped_title)
-    title_font_size = 16
-    if title_size <= 40:
-        title_font_size = 12
-    fig1.update_layout(yaxis_title=traffic)
+    # Wrap the title text for better readability
+    wrapped_title = '<br>'.join(textwrap.wrap(title, width=title_size))
+
+    # Adjust title font size based on title size
+    title_font_size = 16 if title_size <= 40 else 12
+
+    # Update the layout and title
     fig1.update_layout(
-        title_text=wrapped_title_with_br,
+        yaxis_title=traffic,
+        title_text=wrapped_title,
         xaxis_title_text='Time interval',
-    )
-    fig1.update_layout(
         xaxis=dict(
             tickmode='array',
             tickvals=mean_street_data_without.index,
             ticktext=list_timeframe_string
-        )
+        ),
+        title={'y': 0.95, 'pad': {'b': 50}},
+        title_font_size=title_font_size,
+        template='plotly_dark',
+        font=dict(color='#deb522')
     )
-    fig1.update_layout(title={'y': 0.95, 'pad': {'b': 50}}, title_font_size=title_font_size)
-    fig1.update_layout(template='plotly_dark', font=dict(color='#deb522'))
     return fig1
